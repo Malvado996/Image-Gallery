723ca24261b8f374380f7802ce1fbca8dc0e0a24
Philip Rodrigues <37449516+Malvado996@users.noreply.github.com>
2019-06-03 09:43:41 -0400

    pre lightbox draft
    last update before lightbox

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+
 // Global variables for live search function ----------------------------
 const searchBar = document.getElementById('searchBar');
 const searchable = document.getElementsByClassName('searchable');
@@ -6,7 +7,7 @@ console.log(searchable);
 //-------- Displays searchBar only if Js is intitiated-------------------
 searchBar.style.visibility = 'visible';
 
-// ------- Live Search Function -----------------------------------------
+// ------- Live Search Function Start-----------------------------------------
 
 // Adds eventListener
 searchBar.addEventListener('input', () => {
@@ -17,17 +18,19 @@ console.log(dataSearch);
 
 // Activates for loop only if atleast 3 characters are entered
   if (dataSearch.length >= 3 ) {
+        console.log('****SEARCH START****');
     for (i = 0; i < searchable.length; i += 1) {
         // Converts captured elements text in lowercase
         let searchText = searchable[i].textContent.toLowerCase();
         console.log(searchText);
         // Compares element text to user input
         if (searchText.includes(dataSearch)) {
-          console.log('+++++');
+          console.log('++++++++');
         } else {
-          console.log('-----');
+          console.log('--------');
         }
     }
+    console.log('****SEARCH END*****');
   } else {
         // Resets elements when user input falls below 3 characters
         if (dataSearch.length <= 3 ) {
