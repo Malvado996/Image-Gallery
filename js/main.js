f666c0ac98df8328eaef7111037ca6cefd83eeed
Philip Rodrigues <37449516+Malvado996@users.noreply.github.com>
2019-06-04 17:29:55 -0400

    set sticker width to .header

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 
+// Live Search -----------------------------------------------------------------
+
 const searchBar = document.getElementById('searchBar');
 searchBar.style.visibility = 'visible';
 
@@ -16,9 +18,9 @@ searchBar.addEventListener('keyup', () => {
             let text = card[i].innerText.toLowerCase();
             let x = text.includes(input);
               if (x) {
-                card[i].style.color = 'green';
+                card[i].style.display = 'block';
               } else {
-                card[i].style.color = 'red';
+                card[i].style.display = 'none';
               }
         }
     } else
@@ -26,63 +28,12 @@ searchBar.addEventListener('keyup', () => {
    if (input.length <= 2) {
         console.log('START B');
         for (i = 0; i < card.length; i += 1) {
-          card[i].style.color = 'yellow';
+          card[i].style.display = 'block';
     }
   }
 });
 
-
-// // Global variables for live search function ----------------------------
-// const searchBar = document.getElementById('searchBar');
-// const searchable = document.getElementsByClassName('searchable');
-//
-//
-//
-// //-------- Displays searchBar only if Js is intitiated-------------------
-// searchBar.style.visibility = 'visible';
-//
-// // ------- Live Search Function Start-----------------------------------------
-//
-// // Adds eventListener
-// searchBar.addEventListener('input', () => {
-//
-// // Captures user input and converts to lowercase text
-//
-// let dataSearch = searchBar.value.toLowerCase();
-// console.log(dataSearch);
-// console.log(dataSearch.length);
-// // Activates for loop only if atleast 3 characters are entered
-//   if (dataSearch.length >= 3 ) {
-//         console.log('****SEARCH START****');
-//     for (i = 0; i < searchable.length; i += 1) {
-//         let searchText = searchable[i].textContent.toLowerCase();
-//         console.log(searchText);
-//         // Compares element text to user input
-//
-//         searchable[i].parentNode.parentNode.parentNode.style.display = 'none';
-//         if (searchText.includes(dataSearch)) {
-//           console.log('++++++++');
-//           searchable[0].parentNode.parentNode.parentNode.style.display = 'block';
-//           console.log(searchable[0].parentNode.parentNode.parentNode);
-//         }
-//
-//
-//      else {
-//         if (dataSearch.length <= 3 ) {
-//           let card = document.getElementsByClassName('card item');
-//           for ( i = 0; i < card.length; i += 1) {
-//           card[i].style.display = 'block';
-//          }
-//         }
-//   }
-// }
-// }
-// });
-// ----------- Live Search Function end -------------------------------
-
-
-//console.log(searchable[i].parentNode.parentNode.parentNode);
-//searchable[i].parentNode.parentNode.parentNode.style.display = 'none';
+// ------- Live Search Function Start-----------------------------------------
 
 // Lightbox---------------------------------------------------------------------
 $('#selector1').lightGallery({
@@ -94,4 +45,5 @@ $('#selector1').lightGallery({
 $("#sticker").sticky({
   topSpacing:0,
   zIndex:10,
+  getWidthFrom: '.header'
 });
